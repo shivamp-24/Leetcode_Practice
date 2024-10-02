278087d6f4c112254730f2b08772e2cc8c71b0c6
shivamp_24 <122880663+shivamp-24@users.noreply.github.com>
2024-10-02 17:35:14 +0530

    Time: 51 ms (21.37%), Space: 42.7 MB (15.76%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
@@ -4,17 +4,17 @@ public:
         //2 cases
         //normal kadane
         //kadane to find minimum subarray sum
-        int maxi=nums[0], mini=nums[0];
+        int maxi=INT_MIN, mini=INT_MAX;
         int sum1=0, sum2=0, sum=0;
         for(auto &it: nums){
             //normal
-            if(sum1<0) sum1=0;
             sum1+=it;
             maxi=max(maxi, sum1);
+            if(sum1<0) sum1=0;
             //mini
-            if(sum2>0) sum2=0;
             sum2+=it;
             mini=min(mini, sum2);
+            if(sum2>0) sum2=0;
             sum+=it;
         }
         if(sum==mini) return maxi;
