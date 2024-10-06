abffe738e3f0c90c9dc7edf769d8471818b82ef4
shivamp_24 <122880663+shivamp-24@users.noreply.github.com>
2024-10-06 14:55:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (35.48%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    bool areSentencesSimilar(string s1, string s2) {
-        // Convert sentences to lists of words
-        stringstream ss1(s1), ss2(s2);
-        string word;
-        vector<string> s1Words, s2Words;
-        while (ss1 >> word) s1Words.push_back(word);
-        while (ss2 >> word) s2Words.push_back(word);
-
-        int start = 0, ends1 = s1Words.size() - 1, ends2 = s2Words.size() - 1;
-
-        // If words in s1 are more than s2, swap them and return the answer.
-        if (s1Words.size() > s2Words.size()) return areSentencesSimilar(s2, s1);
-
-        // Find the maximum words matching from the beginning.
-        while (start < s1Words.size() && s1Words[start] == s2Words[start])
-            ++start;
-
-        // Find the maximum words matching in the end.
-        while (ends1 >= 0 && s1Words[ends1] == s2Words[ends2]) {
-            --ends1;
-            --ends2;
+    bool areSentencesSimilar(string sentence1, string sentence2) {
+        deque<string> v1, v2;
+        stringstream ss(sentence1);
+        string s;
+        while(ss>>s){
+            v1.push_back(s);
         }
-
-        // If ends1 index is less than start, then sentence is similar.
-        return ends1 < start;
+        stringstream ss2(sentence2);
+        while(ss2>>s){
+            v2.push_back(s);
+        }
+        if(v1.size() > v2.size()) swap(v1,v2);
+        while(v1.size()){
+            if(v1.front() == v2.front()){
+                v1.pop_front();
+                v2.pop_front();
+            } else if(v1.back() == v2.back()){
+                v1.pop_back();
+                v2.pop_back();
+            } else {
+                break;
+            }
+        }
+        return v1.empty();
     }
 };
