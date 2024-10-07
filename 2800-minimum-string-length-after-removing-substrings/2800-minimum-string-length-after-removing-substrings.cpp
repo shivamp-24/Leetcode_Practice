09e30f59389130f3b8f0645f4b51670ee84c2ef4
shivamp_24 <122880663+shivamp-24@users.noreply.github.com>
2024-10-07 17:22:28 +0530

    Time: 8 ms (42.58%), Space: 11.5 MB (72.37%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     int minLength(string s) {
         int n=s.size();
-        while(true){
-            string tp;
-            for(int i=0;i<s.size();){
-                if(s[i]=='A' && s[i+1]=='B') i+=2;
-                else if(s[i]=='C' && s[i+1]=='D') i+=2;
-                else{
-                    tp.push_back(s[i++]);
-                }
-            }
-            if(tp.size()==s.size()) return s.size();
-            s=tp;
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            if(!st.empty() && ((st.top()=='A' && s[i]=='B') || (st.top()=='C' && s[i]=='D'))) st.pop();
+            else st.push(s[i]);
         }
-        return s.size();
+        return st.size();
     }
 };
